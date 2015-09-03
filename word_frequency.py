965c519a309d48dd40c33048b57fa2410d952da0
Glenn Hurley <glenn.hurley@gmail.com>
2015-09-03 16:55:01 -0400

    Created a function to pass the functional tests that were given, as well as one to pass the top 20 requirement.

--- word_frequency.py
+++ word_frequency.py
@@ -1,2 +1,32 @@
+import re
+
+
 def word_frequency(text):
-    return {"hello": 1}
+    words = {}
+    for word in text.split():
+        word = re.sub(r'[^A-Za-z]', '', word).lower()
+        if word in words:
+            words[word] += 1
+        else:
+            words[word] = 1
+    return words
+
+
+def top_20(words):
+    for k, v in sorted(words.items(), key=lambda x: x[1], reverse=True)[:20]:
+        print(k, v)
+
+
+def get_text():
+    user_file = input("Which file: ")
+    in_file = open(user_file)
+    text = in_file.read()
+    return text
+
+
+def main():
+    top_20(word_frequency(get_text()))
+
+
+if __name__ == '__main__':
+    main()
