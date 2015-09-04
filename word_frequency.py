8f8fe37e61494857c97e83398b186f5a0bb5691d
Glenn Hurley <glenn.hurley@gmail.com>
2015-09-03 22:06:10 -0400

    Nightmare mode attempt. Added a requirements file for the use of tabulate.

--- word_frequency.py
+++ word_frequency.py
@@ -1,6 +1,10 @@
+# http://stackoverflow.com/questions/10079216/skip-first-entry-in-for-loop-in-python
+
 import re
 
-frequent = ['a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as', 'at', 'be', 'because', 'been', 'but', 'by', 'can', 'cannot', 'could', 'dear', 'did', 'do', 'does', 'either', 'else', 'ever', 'every', 'for', 'from', 'get', 'got', 'had', 'has', 'have', 'he', 'her', 'hers', 'him', 'his', 'how', 'however', 'i', 'if', 'in', 'into', 'is', 'it', 'its', 'just', 'least', 'let', 'like', 'likely', 'may', 'me', 'might', 'most', 'must', 'my', 'neither', 'no', 'nor', 'not', 'of', 'off', 'often', 'on', 'only', 'or', 'other', 'our', 'own', 'rather', 'said', 'say', 'says', 'she', 'should', 'since', 'so', 'some', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 'they', 'this', 'tis', 'to', 'too', 'twas', 'us', 'wants', 'was', 'we', 'were', 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'would', 'yet', 'you', 'your']
+from tabulate import tabulate
+
+frequent = ['', 'a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as', 'at', 'be', 'because', 'been', 'but', 'by', 'can', 'cannot', 'could', 'dear', 'did', 'do', 'does', 'either', 'else', 'ever', 'every', 'for', 'from', 'get', 'got', 'had', 'has', 'have', 'he', 'her', 'hers', 'him', 'his', 'how', 'however', 'i', 'if', 'in', 'into', 'is', 'it', 'its', 'just', 'least', 'let', 'like', 'likely', 'may', 'me', 'might', 'most', 'must', 'my', 'neither', 'no', 'nor', 'not', 'of', 'off', 'often', 'on', 'only', 'or', 'other', 'our', 'own', 'rather', 'said', 'say', 'says', 'she', 'should', 'since', 'so', 'some', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'these', 'they', 'this', 'tis', 'to', 'too', 'twas', 'us', 'wants', 'was', 'we', 'were', 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'would', 'yet', 'you', 'your']
 
 
 def word_frequency(text):
@@ -17,8 +21,14 @@ def word_frequency(text):
 
 
 def top_20(words):
-    for k, v in sorted(words.items(), key=lambda x: x[1], reverse=True)[:20]:
-        print(k, v)
+    top_list = sorted(words.items(), key=lambda x: x[1], reverse=True)[:20]
+    # http://stackoverflow.com/questions/14831830/convert-a-list-of-tuples-to-a-list-of-lists
+    top_list = [list(elem) for elem in top_list]
+    top_div = top_list[0][1]//50
+    top_list[0][1] = '#' * 50
+    for word in top_list[1:]:
+        word[1] = '#' * ((word[1] - 50)//top_div)
+    print(tabulate(top_list))
 
 
 def get_text():
